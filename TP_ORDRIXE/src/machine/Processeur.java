972cc82748658b6e61e00bda7130910cfe5a3a15
secu2 <clementbrocard@free.fr>
2014-03-31 10:07:23 +0200

    modif

--- TP_ORDRIXE/src/machine/Processeur.java
+++ TP_ORDRIXE/src/machine/Processeur.java
@@ -1,5 +1,8 @@
 package machine;
 
 public class Processeur {
-	
+	Memoire memoire;
+	public Processeur(){
+		memoire = new Memoire();
+	}
 }
--- TP_ORDRIXE/src/systeme/Jeu.java
+++ TP_ORDRIXE/src/systeme/Jeu.java
@@ -1,5 +1,7 @@
 package systeme;
 
 public class Jeu {
-
+	public static void main(String[] args){
+		//Main class
+	}
 }
