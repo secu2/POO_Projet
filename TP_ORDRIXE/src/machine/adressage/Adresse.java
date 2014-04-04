09838952ec0ae42b913c848663003206612d2bc9
secu2 <clementbrocard@free.fr>
2014-04-04 11:54:50 +0200

    Ajout de commentaires

--- TP_ORDRIXE/src/machine/adressage/Adresse.java
+++ TP_ORDRIXE/src/machine/adressage/Adresse.java
@@ -1,7 +1,13 @@
 package machine.adressage;
-
+/**
+ * Outil d'adressage
+ */
 public abstract class Adresse {
 	protected int adresse;
+	/**
+	 * Permet de récupérer l'adressage immédiat
+	 * @return l'adresse
+	 */
 	public int adresse(){
 		return adresse;
 	}
--- TP_ORDRIXE/src/machine/adressage/RelatifIndirect.java
+++ TP_ORDRIXE/src/machine/adressage/RelatifIndirect.java
@@ -3,6 +3,15 @@ package machine.adressage;
 import machine.Memoire;
 
 public class RelatifIndirect extends Adresse {
+	/**
+	 * Permet de contruire une adresse à partir d'un adressage relatif,
+	 * l'adresse et trouvée dans l'opérande droit de l'instruction située dans
+	 * la case mémoire désignée par l'adresse+offset
+	 * Nécéssite une accès à la mémoire pour récupérer la position de l'adresse
+	 * @param memoire
+	 * @param adresse
+	 * @param offset
+	 */
 	public RelatifIndirect(Memoire memoire, int adresse, int offset){
 		this.adresse = memoire.lire(adresse+offset).operandes().operandeDroit().adresse();
 	}
